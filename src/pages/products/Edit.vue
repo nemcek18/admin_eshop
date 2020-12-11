2c343e2ab3ceb9d84531d250f44e9c17628ba66c
Matus Stas <matusstas.ms@gmail.com>
2020-12-11 20:15:32 +0100

    edit - nacitanie udajov

--- src/pages/products/Edit.vue
+++ src/pages/products/Edit.vue
@@ -1,34 +1,78 @@
 <template>
 <div class="q-my-xl">
     <q-card>
-        <q-card-title>Create new product</q-card-title>
+        <q-card-title>Edit</q-card-title>
         <q-card-main>
-            <q-field :count="250">
-                <q-input float-label="Name" max-length="250" />
+            <q-field :count="30">
+                <q-input float-label="Brand" v-model="productBrand" max-length="30" />
             </q-field>
-            <q-field :count="5000">
+            <q-field :count="30">
+                <q-input float-label="Model" v-model="productModel" max-length="30" />
+            </q-field>
+            <q-field :count="500">
                 <q-input
-                    type="textarea"
                     float-label="Description"
+                    v-model="productDescription"
+                    type="textarea"
                     :max-height="100"
                     rows="7"
                 />
             </q-field>
-            <q-field helper="Supported format: JPG, max. file size: 300KiB" class="q-mt-lg">
-                <q-uploader float-label="Images" multiple extensions=".jpg" auto-expand/>
-            </q-field>
+            <!-- <q-field>
+                <q-input float-label="Price" v-model.number="productPrice" type="number" />
+            </q-field> -->
         </q-card-main>
         <q-card-actions class="q-mt-md">
             <div class="row justify-end full-width docs-btn">
                 <q-btn label="Cancel" flat to="/products/index"/>
-                <q-btn label="Create" color="primary" />
+                <q-btn label="Update" color="primary" @click="updateProduct"/>
             </div>
         </q-card-actions>
     </q-card>
 </div>
 </template>
 
-<style lang="stylus">
-.docs-btn .q-btn
-    padding 15px 20px
-</style>
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      productBrand: '',
+      productModel: '',
+      productDescription: ''
+    }
+  },
+  methods: {
+    updateProduct () {
+      axios
+        .put(`http://127.0.0.1:8000/api/products/` + this.$route.params.id, this.productData)
+        .then(response => {
+          this.$q.notify({ type: 'positive', timeout: 2000, message: 'The product has been updated.' })
+        })
+        .catch(error => {
+          this.$q.notify({ type: 'negative', timeout: 2000, message: 'An error has been occured.' })
+          console.log(error)
+        })
+    }
+  },
+  mounted () {
+    axios
+      .get(`http://127.0.0.1:8000/api/products/` + this.$route.params.id + '/edit')
+      .then(response => {
+        this.productBrand = response.data.brand
+        this.productModel = response.data.model
+        this.productDescription = response.data.description
+      })
+      .catch(error => {
+        this.$q.notify({ type: 'negative', timeout: 2000, message: 'Loading product: an error has been occured.' })
+        console.log(error)
+      })
+  },
+  computed: {
+    productData: function () {
+      return { brand: this.productBrand, model: this.productModel, description: this.productDescription }
+    }
+  }
+}
+</script>
